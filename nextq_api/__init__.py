5f8d94a492f6769b3b8717d2732261c77c0614ba
Aiman Farhan <aimanfaruk98@gmail.com>
2021-01-29 07:25:33 +0800

    added blueprint structure

---
+++ nextq_api/__init__.py
@@ -0,0 +1,9 @@
+from app import app, csrf
+from flask_cors import CORS
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+## API Routes ##
+from nextq_api.blueprints.users.views import users_api_blueprint
+
+app.register_blueprint(nextq_api_blueprint, url_prefix='/api/nextq/users')
--- server.py
+++ server.py
@@ -25,42 +25,6 @@ def migrate():
 def index():
     return render_template('index.html')
 
-#USER API DRAFT
-@app.route('/user/new', methods=['POST'])
-def new_user():
-    params = request.json
-    new_user = User(
-        username = params.get("username"), 
-        email=params.get("email"), 
-        password=params.get("password"),
-        mobile_number=params.get("mobilehp")
-        )
-
-    if new_user.save():
-        token = create_access_token(identity = new_user.id)
-        return jsonify({"token":token})
-    else:
-        return jsonify([err for err in new_user.errors])
-
-@app.route('/user/<user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.get_or_none(user.id == user_id)
-
-    if user:
-        token = create_access_token(identity = user.id)
-        return jsonify({"token":token})
-    else:
-        return jsonify([err for err in new_user.errors])
-
-@app.route('/user/<user_id>/checkin', methods=['POST'])
-def user_checkin(user_id):
-    user = User.get_or_none(user.id == user_id)
-
-    if user:
-        pass
-    else:
-        pass
-
 #SHOP API DRAFT
 @app.route('/shop/new', methods=['POST'])
 def new_shop():
