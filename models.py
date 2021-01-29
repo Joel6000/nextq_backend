969827bc17f6b9ae198c341aa3da7ba77dbe8240
fabianlim <flqf1217@hotmail.com>
2021-01-29 11:14:15 +0800

    added databade models

--- models.py
+++ models.py
@@ -3,12 +3,15 @@ import peewee as pw
 import datetime
 from playhouse.postgres_ext import PostgresqlExtDatabase
 
+
+contacts_db = SqliteDatabase('contacts.db')
+
 db = PostgresqlExtDatabase(os.getenv('DATABASE'))
 
-class BaseModel(pw.Model):
+class Base(pw.Model):
     created_at = pw.DateTimeField(default=datetime.datetime.now)
     updated_at = pw.DateTimeField(default=datetime.datetime.now)
-
+    
     def save(self, *args, **kwargs):
         self.updated_at = datetime.datetime.now()
         return super(BaseModel, self).save(*args, *kwargs)
@@ -17,19 +20,24 @@ class BaseModel(pw.Model):
         database = db
         legacy_table_names = False
 
-class User(BaseModel, name, age, mobile):
-    pass
 
-class Shop(BaseModel):
-    name = pw.CharField(unique=True)
-    customer_limit = None
+class User(Base):
+    name = pw.CharField()
+    age = pw.IntegerField()
+    mobile = pw.IntegerField(unique=True)
 
-class History(BaseModel):
-    store = pw.ForeignKeyField(Store, backref='warehouses')
-    location = pw.TextField()
+class Store(Base):
+    name = pw.CharField()
+    location = pw.CharField()
+    customer_limit = pw.IntegerField(null=True)
 
-class Product(BaseModel):
-    name = pw.CharField(index=True)
-    description = pw.TextField()
-    warehouse = pw.ForeignKeyField(Warehouse, backref='products')
-    color = pw.CharField(null=True)
+class History(Base):
+    user = pw.ForeignKeyField(User)
+    store = pw.ForeignKeyField(Store)
+    time_in = pw.DateTimeField(default=datetime.datetime.now)
+    time_out = pw.DateTimeField(default=None)
+
+class Queue(Base):
+    user = pw.ForeignKeyField(User)
+    store = pw.ForeignKeyField(Store)
+    queue_time = pw.DateTimeField(default=datetime.datetime.now)
