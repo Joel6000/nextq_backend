a639506d720a0964f0a0d8f06913c8daff8e60ce
Aiman Farhan <aimanfaruk98@gmail.com>
2021-01-31 15:00:28 +0800

    changed url prefixes

--- nextq_api/__init__.py
+++ nextq_api/__init__.py
@@ -8,5 +8,5 @@ from nextq_api.blueprints.users.views import users_api_blueprint
 from nextq_api.blueprints.stores.views import stores_api_blueprint
 
 
-app.register_blueprint(users_api_blueprint, url_prefix='/api/nextq/users')
-app.register_blueprint(stores_api_blueprint, url_prefix='/api/nextq/stores')
+app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
+app.register_blueprint(stores_api_blueprint, url_prefix='/api/v1/stores')
