2b9cbc1719734761528e68cbac4126c936041605
Aiman Farhan <aimanfaruk98@gmail.com>
2021-02-01 08:27:54 +0800

    login

--- nextq_api/blueprints/auth/views.py
+++ nextq_api/blueprints/auth/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request, Response
 from flask_jwt_extended import create_access_token, jwt_required
 from werkzeug.security import check_password_hash
 from models.user import User
+import datetime
 
 
 auth_api_blueprint = Blueprint('auth_api',
@@ -10,14 +11,12 @@ auth_api_blueprint = Blueprint('auth_api',
 
 @auth_api_blueprint.route('/user', methods=['GET'])
 def login():
-    user = User.get_or_none(User.id == user_id)
-    if user:
-        token = create_access_token(identity = user.id)
-        return jsonify({
-            "username":user.name,
-            "mobile":user.mobile,
-            "email":user.email,
-            "token":token
-            })
-    else:
-        return jsonify([err for err in new_user.errors])
+    params = request.json
+    user = User.get_or_none(User.mobile == params.get('mobile'))
+    authorised = user.check_password_hash(params.get('password'))
+    if not authorised:
+        return {'error': 'Email or password invalid'}
+    
+    expires = datetime.timedelta(days=1)
+    access_token = create_access_token(identity=user.id), expires_delta = expires)
+    return {'token': access_token}
