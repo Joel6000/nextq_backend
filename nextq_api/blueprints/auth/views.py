b5d915143abc33c97eb07594a52f4a85c9ba3266
Aiman Farhan <aimanfaruk98@gmail.com>
2021-02-01 08:45:03 +0800

    added login api

--- nextq_api/blueprints/auth/views.py
+++ nextq_api/blueprints/auth/views.py
@@ -9,14 +9,16 @@ auth_api_blueprint = Blueprint('auth_api',
                              __name__,
                              template_folder='templates')
 
-@auth_api_blueprint.route('/user', methods=['GET'])
+@auth_api_blueprint.route('/user', methods=['POST'])
 def login():
     params = request.json
     user = User.get_or_none(User.mobile == params.get('mobile'))
-    authorised = user.check_password_hash(params.get('password'))
-    if not authorised:
-        return {'error': 'Email or password invalid'}
+    if user:
+        authorised = check_password_hash(user.password_hash,params.get('password'))
+        if  authorised:
+            access_token = create_access_token(identity = str(user.id))
+            return {'token': access_token}
+        else:
+            return {'error': 'Email or password invalid'}
     
-    expires = datetime.timedelta(days=1)
-    access_token = create_access_token(identity=user.id), expires_delta = expires)
-    return {'token': access_token}
+        
