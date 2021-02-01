798e157de6b693aeea8e0b16a92c846b0e033ac3
Aiman Farhan <aimanfaruk98@gmail.com>
2021-02-02 07:16:06 +0800

    created queue blueprint

--- nextq_api/__init__.py
+++ nextq_api/__init__.py
@@ -8,9 +8,12 @@ from nextq_api.blueprints.users.views import users_api_blueprint
 from nextq_api.blueprints.stores.views import stores_api_blueprint
 from nextq_api.blueprints.history.views import history_api_blueprint
 from nextq_api.blueprints.auth.views import auth_api_blueprint
+from nextq_api.blueprints.queue.views import queue_api_blueprint
+
 
 
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
 app.register_blueprint(stores_api_blueprint, url_prefix='/api/v1/stores')
 app.register_blueprint(history_api_blueprint, url_prefix='/api/v1/history')
 app.register_blueprint(auth_api_blueprint, url_prefix='/api/v1/auth')
+app.register_blueprint(queue_api_blueprint, url_prefix='/api/v1/queue')
