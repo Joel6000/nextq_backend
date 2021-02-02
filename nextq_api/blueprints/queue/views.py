ae2b1541dd81320e11e1e29ed878b7a7ffa7e207
Aiman Farhan <aimanfaruk98@gmail.com>
2021-02-02 15:39:14 +0800

    building queue api

--- nextq_api/blueprints/queue/views.py
+++ nextq_api/blueprints/queue/views.py
@@ -13,4 +13,32 @@ queue_api_blueprint = Blueprint('queue_api',
 
 @queue_api_blueprint.route('/<user_id>/user/<store_id>/store', methods=['POST'])
 def create(user_id, store_id):
-    pass
+    store = Store.get_by_id(store_id)
+    user = User.get_by_id(user_id)
+
+    space_available = int(store.limit - shop.headcount)
+
+    if space_available < 0:
+        new_queue = Queue(
+            user=user,
+            store=store
+        )
+
+        if new_queue.save():
+            return jsonify({
+                "user":new_queue.store,
+                "store":new_queue.store
+            })
+        else:
+            return jsonify([err for err in new_history.errors])
+    else:
+        return jsonify({"errors":"Space is available, queue function skipped."})
+
+
+
+# query = queue.select().where(shop.id = blabla) → [1, a, cbde, were, 2325, 6, 7, 8, 9, 10]
+# notify(space_available, query)
+
+# def notify(space_available, query):
+#     for i in range(0:space_available):
+#         alert(query[i+5])
