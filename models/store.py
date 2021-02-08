5fed50fc3b58ab452a84299eb542bf953f8a0099
AimanFarhanMohdFaruk <aimanfaruk98@gmail.com>
2021-02-08 11:05:32 +0800

    added image_url

--- models/store.py
+++ models/store.py
@@ -8,3 +8,4 @@ class Store(BaseModel):
     customer_limit = pw.IntegerField(null=True) #set default == 10
     headcount = pw.IntegerField(default = 0)
     queue = pw.IntegerField(default = 0)
+    image_url = pw.TextField(null=True, default = "https://static.thenounproject.com/png/1369734-200.png" )
